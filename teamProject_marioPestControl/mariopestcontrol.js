8a89c22432397e87716e6422b65654b8f0747fb3
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2019-11-21 16:24:35 -0500

    basic files and imgs

---
+++ teamProject_marioPestControl/mariopestcontrol.js
@@ -0,0 +1,60 @@
+// Pricing:
+// Goombas: 5 Coins
+// Bob-ombs: 7 Coins
+// Cheep-cheeps: 11 Coins
+
+//I always put snippets of the instructions in comments 
+//to make sure i don't forget anything. Is that fine with you?
+
+//Also I usually create all of my html right here too. 
+//Because i don't like how to select by class or tag creates arrays. 
+//And i personally think IDs should be for specific things.
+//But i can work either way. 
+//Which method do you prefer? 
+
+//I would create a loop that creates all elements necessary for each pest. 
+//I never create them individually. 
+//we can use if statements or an extra array to apply specific classes or ids for selecting  later. 
+//We can also use a class constructor to create the baddies. 
+//Each pest will have the same requirements, 
+//so looks like they would be perfect for each being an object with common properties. 
+//or we can do them all in html?
+
+// A large title at the top of the page entitled "Mario Pest Control"
+/*<main> <h1> </h1> <form> <ul>
+        <li>
+            <h2>Name</h2>
+            <img src="">
+            <p>Price: </p>
+            <label>Amount Caught: <input></label>
+            <h3> Price of this type goes here, based on its amount </h3>
+        </li>
+        <li>
+                <h2>Name</h2>
+                <img src="">
+                <p>Price: </p>
+                <label>Amount Caught: <input></label>
+                <h3> Price of this type goes here, based on its amount </h3>
+        </li>
+        <li>
+                <h2>Name</h2>
+                <img src="">
+                <p>Price: </p>
+                <label>Amount Caught: <input></label>
+                <h3> Price of this type goes here, based on its amount </h3>
+        </li>
+    </ul> 
+    <button>Add All! </button>
+    <h2>innerHTML can change to match sum of all. 
+    Or my prefered which is to have it create a heading with the textContent of the result</h2>
+    </form> </main>*/
+
+
+//And then footer. 
+// A footer showing:
+// Mario's email address
+// Mario's company website url
+// Mario's physical address
+
+
+//For the EXTRA CREDIT, we can add media queries in CSS.
